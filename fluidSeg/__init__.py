2dc1389c9121af7dfcc039859429b0a8d66e2702
Sean Tseng <seantyh@gmail.com>
2017-09-25 20:41:12 +0800

    fix FluidSeg directory name

--- fluidSeg/__init__.py
+++
@@ -1,4 +0,0 @@
-from .segdata import Segments
-from .segment import FluidSeg
-from .tokendata import TokenData
-from .lexicon import Lexicon, LexiconFactory
--- fluidSeg/tokendata.py
+++
@@ -1,8 +0,0 @@
-class TokenData:
-    def __init__(self, txt, start, end):
-        self.text = txt
-        self.chstart = start
-        self.chend = end
-
-    def __repr__(self):
-        return "<{}-{},{}>".format(self.text, self.chstart, self.chend)
